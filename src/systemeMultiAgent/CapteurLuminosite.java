e962aff7d2e89cfcdcbdeae066ef95df8757fadc
bensalah-Roukaya <bensalahroukaya@gmail.com>
2018-10-25 10:15:35 +0200

    modifications

--- src/systemeMultiAgent/CapteurLuminosite.java
+++ src/systemeMultiAgent/CapteurLuminosite.java
@@ -6,37 +6,15 @@ public class CapteurLuminosite {
     int n;
 
 
-    public CapteurLuminosite(int n) {
+    /*public CapteurLuminosite(int n) {
         this.n = n;
+    }*/
+    public int Valeur ()
+    {
+        Random rand = new Random();
+        n = rand.nextInt(20) + 1;
+        System.out.println("Luminosité = " + n);
+        return n;
     }
- public int Valeur ()
- {
-     Random rand = new Random();
-     n = rand.nextInt(50) + 1;
-
-     System.out.println("Luminosité = " + n);
-     return n;
- }
-
-}
-
-/*
-    public synchronized boolean tryTake(AgentLumiereIntelligente asker) {
-        if (takenBy != null)
-            return false;
-        takenBy = asker;
-        return true;
-    }
-
-    public synchronized void release(AgentLumiereIntelligente asker) {
-        if (takenBy == asker) {
-            takenBy = null;
-        }
-    }
-
-    public synchronized boolean owned(AgentLumiereIntelligente asker) {
-        return takenBy == asker;
-    }
-
 
 }
